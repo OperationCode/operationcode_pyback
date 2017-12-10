6040acacb27fa95cd8098a52f529f87b77adc8e4
wimo7 <wimo7083@colorado.edu>
2017-12-10 15:16:48 -0700

    Added message json format.

---
+++ src/message.py
@@ -0,0 +1,14 @@
+# https://api.slack.com/docs/messages/builder
+new_join = {
+     "text": (
+            "Hi {{ real_name }},\n\n Welcome to Operation Code! I'm a bot designed to help answer questions and get you on your way in our community.\n\n"
+            "Please take a moment to review our <https://op.co.de/code-of-conduct|Code of Conduct.>\n\n"
+            "Our goal here at Operation Code is to get veterans and their families started on the path to a career in programming. "
+            "We do that through providing you with scholarships, mentoring, career development opportunities, conference tickets, and more!\n\n"
+            "You're currently in Slack, a chat application that serves as the hub of Operation Code. "
+            "If you're currently visiting us via your browser, Slack provides a stand alone program to make staying in touch even more convenient. "
+            "You can download it <https://slack.com/downloads|here.>\n\n"
+            "Want to make your first change to a program right now? "
+            "All active Operation Code Projects are located on our source control repository. "
+            "Our projects can be viewed on <https://github.com/OperationCode/START_HERE|Github.>")
+     }
