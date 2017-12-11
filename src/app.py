6c82feeae8128d7ef2af2305db4963dd4007c02f
William Montgomery <wimo7083@colorado.edu>
2017-12-11 12:30:58 -0700

    Added restart connection, test route, proxy info, channel join method.

--- src/app.py
+++ src/app.py
@@ -2,7 +2,8 @@ import logging
 import time
 from slackclient import SlackClient
 from utils.log_manager import setup_logging
-from archived.creds import TOKEN
+from src.creds import TOKEN, PROXY
+import traceback
 
 logger = logging.getLogger(__name__)
 new_event_logger = logging.getLogger(f'{__name__}.new_member')
@@ -21,7 +22,7 @@ MESSAGE = (
     "All active Operation Code Projects are located on our source control repository. "
     "Our projects can be viewed on <https://github.com/OperationCode/START_HERE|Github.>")
 
-slack_client = SlackClient(TOKEN)
+slack_client = SlackClient(TOKEN, proxies=PROXY)
 
 
 def build_message(message_template, **kwargs):
@@ -32,12 +33,18 @@ def event_handler(event_dict):
     all_event_logger.info(event_dict)
     if event_dict['type'] == 'team_join':
         new_event_logger.info('New member event recieved')
+        new_member(event_dict)
 
-        # new_member(event_dict)
+    if event_dict['type'] == 'presence_change':
+        all_event_logger.info('User {} changed state to {}'.format(user_name_from_id(event_dict['user']), event_dict['presence']))
 
     # can be used for development to trigger the event instead of the team_join
     if event_dict['type'] == 'message' and 'user' in event_dict.keys():
         pass
+    if event_dict['type'] == 'message' and 'user' in event_dict.keys() and event_dict['text'] == 'test4611':
+        event_dict['user'] = {'id': event_dict['user']}
+        new_member(event_dict)
+
 
 
 def new_member(event_dict):
@@ -50,7 +57,7 @@ def new_member(event_dict):
     custom_message = build_message(MESSAGE,
                                    real_name=user_name_from_id(user_id))
 
-    new_event_logger.info('Built message: {}'.format(event_dict))
+    new_event_logger.info('Built message: {}'.format(custom_message))
     slack_client.api_call('chat.postMessage',
                           channel=user_id,
                           text=custom_message,
@@ -81,16 +88,26 @@ def user_name_from_id(user_id):
     else:
         return 'New Member'
 
+def join_channels():
+    response = slack_client.api_call('channels.join', name='general')
+    print(response)
+
+
 
 # set the defalt to a 1 second delay
 def run_bot(delay=1):
     setup_logging()
-
     if slack_client.rtm_connect():
         print(f"StarterBot connected and running with a {delay} second delay")
+
         while True:
-            parse_slack_output(slack_client.rtm_read())
-            time.sleep(delay)
+            try:
+                parse_slack_output(slack_client.rtm_read())
+                time.sleep(delay)
+            except Exception as e:
+                logger.error(f'Some exception occured: {e}')
+                logger.error(f'traceback: {traceback.format_exc(e)}')
+                slack_client.rtm_connect()
     else:
         print("Connection failed.  Invalid Slack token or bot ID")
 
