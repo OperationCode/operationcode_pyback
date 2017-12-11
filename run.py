8e7ab907b38747032eb9345aadf088fe40b22684
wimo7 <wimo7083@colorado.edu>
2017-12-10 20:30:13 -0700

    Added logging reference to main run file.

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
-from src.working_simple_bot import run_bot
+import logging
 
+from src.app import run_bot
+from utils.log_manager import setup_logging
+
+logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
+    setup_logging()
     run_bot()
