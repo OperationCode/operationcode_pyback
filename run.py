c300b8b6b7410040b21a4ed3f4a643a403e976de
wimo7 <wimo7083@colorado.edu>
2017-12-10 21:38:44 -0700

    Removed logging from main caller.

--- run.py
+++ run.py
@@ -1,10 +1,6 @@
 import logging
-
 from src.app import run_bot
-from utils.log_manager import setup_logging
 
-logger = logging.getLogger(__name__)
 
 if __name__ == '__main__':
-    setup_logging()
     run_bot()
