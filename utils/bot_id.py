d53cf9cedecbfbf458ca527e90af75cf67680767
wimo7 <wimo7083@colorado.edu>
2017-12-10 20:28:47 -0700

    Added util to find bot id with name and token

---
+++ utils/bot_id.py
@@ -0,0 +1,19 @@
+import os
+
+from slackclient import SlackClient
+from API_method.creds import TOKEN
+
+BOT_NAME = 'testopcode'
+
+slack_client = SlackClient(TOKEN)
+
+if __name__ == '__main__':
+    api_call = slack_client.api_call('users.list')
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') == BOT_NAME:
+                print(f"Bot ID for {user['name']} is {user.get('id')}")
+    else:
+        print(f"Could not find bot user with the name {BOT_NAME}")
