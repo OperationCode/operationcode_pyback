ed303cdc90f66bf51110de92216257a374e2604f
wimo7 <wimo7083@colorado.edu>
2017-12-26 15:06:12 -0700

    Added exception handling for empty json.

--- src/route_decorators.py
+++ src/route_decorators.py
@@ -2,7 +2,7 @@ import json
 import logging
 from functools import wraps
 
-from flask import make_response, redirect, url_for, request
+from flask import make_response, redirect, request
 
 logger = logging.getLogger(__name__)
 new_event_logger = logging.getLogger(f'{__name__}.new_member')
@@ -12,25 +12,33 @@ def validate_response(json_key, expected_value):
     def decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            data = json.loads(request.form['payload'])[json_key]
-            if data is not expected_value:
-                return redirect(url_for('HTTP403'))
 
-            return func(data, *args, **kwargs)
+            try:
+                data = request.get_json()
+            except Exception as e:
+                return redirect('HTTP400.html', code=400)
+
+            if json_key in data.keys() and data[json_key] != expected_value:
+                return redirect('HTTP403.html', code=403)
+
+            return func(*args, **kwargs)
 
         return wrapper
 
     return decorator
 
 
-def url_verification(f):
-    @wraps(f)
+def url_verification(func):
+    # @wraps(func)
     def decorated_function(*args, **kwargs):
-        data = request.get_json()
+        try:
+            data = request.get_json()
+        except Exception as e:
+            return redirect('HTTP400.html', code=400)
 
-        if data['type'] is 'url_verification':
+        if 'type' in data.keys() and data['type'] == 'url_verification':
             return make_response(json.dumps({'challenge': data['challenge']}), 200)
 
-        return f(*args, **kwargs)
+        return func(*args, **kwargs)
 
     return decorated_function
