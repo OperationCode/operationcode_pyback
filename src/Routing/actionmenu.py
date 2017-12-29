2f587da5eb0c947275efb9a98015089125211b11
William Montgomery <wimo7083@colorado.edu>
2017-12-29 14:39:03 -0700

    First Commit for Routing Interface

---
+++ src/Routing/actionmenu.py
@@ -0,0 +1,311 @@
+from .abc import RouteHandler
+from .utils import get_response_type
+from ..ApiInterface import route_slack
+
+
+
+class ActionMenuHandler(RouteHandler):
+    """
+        Handles the interactive message sent to the #community channel
+        when a new member joins.
+
+        Displays the user that claimed the greeting along with the option
+        to un-claim
+    """
+
+    def __init__(self, *, event_dict):
+        self._user_id = event_dict['user']['id']
+        self._event = event_dict
+        self._click_type = get_response_type(self._event)
+        super().__init__()
+
+    def api_calls(self):
+        pass
+
+    # TODO get resource from clicked event
+    def database_calls(self):
+        if self._click_type == 'resource_buttons':
+            pass
+        pass
+
+    def build_templates(self):
+        trigger_id = self._event['trigger_id']
+
+        # suggestion
+        if self._click_type == 'suggestion':
+            self.text_dict['call'] = 'dialog.open'
+            self.text_dict['dialog'] = SUGGESTION_MODAL
+
+        # mentor
+        elif self._click_type == 'mentor':
+            self.text_dict['call'] = 'dialog.open'
+            self.text_dict['dialog'] = MENTOR_REQUEST_MODAL
+
+        # resources
+        elif self._click_type == 'resource_buttons':
+            self.text_dict['call'] = 'chat.update'
+            self.text_dict['dialog'] = self.make_base_params()
+
+    def build_responses(self):
+        params = self.text_dict['dialog']
+        call = self.text_dict['call']
+
+        self.include_resp(getattr(SlackBuilder, call), self._user_id, **params)
+
+    def make_base_params(self):
+        text = HELP_MENU[self._click_type]
+        return {'text': text,
+                'channel': self._event['channel']['id'],
+                'ts': self._event['message_ts'],
+                'as_user': True
+                }
+
+
+
+SUGGESTION_MODAL = {
+    "callback_id": "suggestion_modal",
+    "title": "Help topic suggestion",
+    "submit_label": "Submit",
+    "elements": [
+        {
+            "type": "text",
+            "label": "Suggestion",
+            "name": "suggestion",
+            "placeholder": "Underwater Basket Weaving"
+        },
+    ]
+}
+
+MENTOR_REQUEST_MODAL = {
+    "callback_id": "mentor_request",
+    "title": "Mentor Service Request",
+    "submit_label": "Submit",
+    "elements": [
+        {
+            "value": "test",
+            "type": "text",
+            "label": "Slack User Name",
+            "name": "Slack User",
+            "placeholder": ""
+        },
+        {
+            "value": "test@test.com",
+            "type": "text",
+            "subtype": "email",
+            "label": "Email",
+            "name": "Email",
+            "placeholder": ""
+        },
+        {
+
+            "type": "select",
+            "label": "Service Type",
+            "name": "service",
+            "placeholder": "Choose a service type",
+            "options": [
+                {
+                    "label": "General Guidance - Voice Chat",
+                    "value": "General Guidance - Voice Chat"
+                },
+                {
+                    "label": "General Guidance - Slack Chat",
+                    "value": "General Guidance - Slack Chat"
+                },
+                {
+                    "label": "Pair Programming",
+                    "value": "Pair Programming"
+                },
+                {
+                    "label": "Code Review",
+                    "value": "Code Review"
+                },
+                {
+                    "label": "Mock Interview",
+                    "value": "Mock Interview"
+                },
+                {
+                    "label": "Resume Review",
+                    "value": "Resume Review"
+                },
+
+            ]
+        },
+        {
+            "type": "select",
+            "label": "Mentor Skillset",
+            "name": "skillset",
+            "optional": "true",
+            "placeholder": "Choose a service type",
+            "options": [
+                {
+                    "label": "Web (Frontend Development)",
+                    "value": "Web (Frontend Development)",
+                },
+                {
+                    "label": "Web (Backend Development)",
+                    "value": "Web (Backend Development)",
+                },
+                {
+                    "label": "Mobile (Android)",
+                    "value": "Mobile (Android)",
+                },
+                {
+                    "label": "Mobile (iOS)",
+                    "value": "Mobile (iOS)",
+                },
+                {
+                    "label": "C / C++",
+                    "value": "C / C++",
+                },
+                {
+                    "label": "C# / .NET",
+                    "value": "C# / .NET",
+                },
+                {
+                    "label": "Data Science",
+                    "value": "Data Science",
+                },
+                {
+                    "label": "DevOps",
+                    "value": "DevOps",
+                },
+                {
+                    "label": "Design / UX",
+                    "value": "Design / UX"
+                },
+                {
+                    "label": "Java",
+                    "value": "Java",
+                },
+                {
+                    "label": "Javascript",
+                    "value": "Javascript",
+                },
+                {
+                    "label": "Python",
+                    "value": "Python",
+                },
+                {
+                    "label": "Ruby / Rails",
+                    "value": "Ruby / Rails",
+                },
+                {
+                    "label": "SQL",
+                    "value": "SQL",
+                },
+            ]
+        },
+        {
+            "type": "textarea",
+            "label": "Additional Details",
+            "name": "Additional Details",
+            "optional": "true",
+            "placeholder": "Please provide us with any more info that may help in us in assigning a mentor to this "
+                           "request. "
+        },
+    ]
+}
+
+
+HELP_MENU = {
+    "text": "Click a button below and info will show up here!",
+    "attachments": [
+        {
+            "text": "",
+            "fallback": "",
+            "color": "#3AA3E3",
+            "callback_id": "greeting_buttons",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "mentor",
+                    "text": "I need a mentor!",
+                    "type": "button",
+                    "value": "mentor",
+
+                },
+                {
+                    "name": "slack",
+                    "text": "Slack Info",
+                    "type": "button",
+                    "value": "slack",
+
+                },
+
+                {
+                    "name": "javascript",
+                    "text": "JavaScript",
+                    "type": "button",
+                    "value": "js_help",
+                },
+                {
+                    "name": "python",
+                    "text": "Python",
+                    "type": "button",
+                    "value": "python_help",
+                },
+                {
+                    "name": "ruby",
+                    "text": "Ruby",
+                    "type": "button",
+                    "value": "ruby_help",
+                },
+
+            ]
+        },
+        {
+            "text": "",
+            "fallback": "",
+            "color": "#3AA3E3",
+            "callback_id": "greeting_buttons",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "suggestion",
+                    "text": "Are we missing something? Click!",
+                    "type": "button",
+                    "value": "suggestion",
+                },
+            ]
+        }
+    ]
+}
+
+def greeted_response_attachments(clicker: str) -> List[dict]:
+    return [
+        {
+            "text": f":100:<@{clicker}> has greeted the new user!:100:",
+            "fallback": "",
+            "color": "#3AA3E3",
+            "callback_id": "greeted",
+            "attachment_type": "default",
+            "actions": [{
+                "name": "reset_greet",
+                "text": f"Reset {clicker}'s claim",
+                "type": "button",
+                "style": "danger",
+                "value": "reset_greet",
+            }]
+        }
+    ]
+
+
+def needs_greet_button() -> List[dict]:
+    return [
+        {
+            'text': "",
+            "fallback": "Someone should greet them!",
+            "color": "#3AA3E3",
+            "callback_id": "greeted",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "greeted",
+                    "text": "I will greet them!",
+                    "type": "button",
+                    "style": "primary",
+                    "value": "greeted",
+                },
+            ]
+        }
+    ]
---
+++ src/Routing/newmember.py
@@ -0,0 +1,168 @@
+from .abc import RouteHandler
+from ..ApiInterface.route_slack import SlackBuilder, Slack
+from keys import COMMUNITY_CHANNEL
+from .utils import needs_greet_button
+
+
+class NewMemberHandler(RouteHandler):
+    """
+        Invoked when a new user joins and a team_join event is received.
+        DMs the new user with the welcome message and help menu as well as pings
+        the #community channel with a new member notification
+        :param event_dict: dict
+
+    """
+
+    def __init__(self, *, event_dict):
+        self.user_id = event_dict['user']['id']
+        self._event = event_dict
+        super().__init__()
+
+    def api_calls(self):
+        # slack api call for user_name
+        self.api_dict['real_name'] = Slack.user_name_from_id(self.user_id)
+
+    # TODO call database
+    def database_calls(self):
+        # database user call
+        # self.db_dict['interests'] = self.db_call_interests()
+        pass
+
+    def process_db_response(self):
+        built_resources = base_resources
+        if not self.db_dict:
+            built_resources['attachments']['actions'] = default_interest
+
+        # TODO process resources
+        else:
+            pass
+        return built_resources
+
+    def build_templates(self):
+        self.text_dict['message'] = text_greet.format(real_name=self.api_dict['real_name'])
+        self.text_dict['resource'] = self.process_db_response()
+        self.text_dict['community'] = f":tada: <@{self.user_id}> has joined! :tada:"
+        self.text_dict['attach'] = needs_greet_button()
+
+    def build_responses(self):
+        message_text = self.text_dict['message']
+        built_resource = self.text_dict['resource']
+        community = self.text_dict['community']
+        attachments = self.text_dict['attachments']
+
+        self.include_resp(SlackBuilder.message, self.user_id, text=message_text)
+        self.include_resp(SlackBuilder.message, self.user_id, **external_buttons)
+        self.include_resp(SlackBuilder.message, self.user_id, **built_resource)
+        self.include_resp(SlackBuilder.message, COMMUNITY_CHANNEL, text=community, attachments=attachments)
+
+
+text_greet = ("Hi {real_name},\n\n Welcome to Operation Code! I'm a bot designed to help answer questions and "
+              "get you on your way in our community.\n\n Our goal here at Operation Code is to get veterans and "
+              "their families started on the path to a career in programming. We do that through providing you with "
+              "scholarships, mentoring, career development opportunities, conference tickets, and more!\n")
+
+external_buttons = {
+    "text": ("The aid we provide requires veteran status *please click the attached link* to verify your status if "
+             "you qualify for optional services.\n\nYou're currently in Slack, a chat application that serves as the "
+             "hub of Operation Code. If you're visiting us via your browser, Slack provides a stand alone program to "
+             "make staying in touch even more convenient.\n\nAll active Operation Code Projects are located on our "
+             "source control repository.Our projects can be viewed on Github\n\n Lastly, please take a moment to "
+             "review our Code of Conduct"),
+    "attachments": [
+        {
+            "text": "",
+            "fallback": "",
+            "color": "#3AA3E3",
+            "callback_id": "external_buttons",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "verify",
+                    "text": "Verify Veteran Status",
+                    "type": "button",
+                    "value": "verify",
+                    "url": "https://operationcode.org/profile",
+                    "style": "primary"
+                },
+                {
+                    "name": "github",
+                    "text": "GitHub",
+                    "type": "button",
+                    "value": "github",
+                    "url": "https://github.com/OperationCode"
+                },
+                {
+                    "name": "download",
+                    "text": "Slack Client",
+                    "type": "button",
+                    "value": "download",
+                    "url": "https://slack.com/downloads"
+                },
+                {
+                    "name": "code_of_conduct",
+                    "text": "Code of Conduct",
+                    "type": "button",
+                    "value": "code_of_conduct",
+                    "url": "https://op.co.de/code-of-conduct"
+                }
+            ]
+        }
+    ]
+}
+
+
+default_interest = [
+    {
+        "name": "javascript",
+        "text": "JavaScript",
+        "type": "button",
+        "value": "javascript"
+    },
+    {
+        "name": "python",
+        "text": "Python",
+        "type": "button",
+        "value": "python"
+    },
+    {
+        "name": "ruby",
+        "text": "Ruby",
+        "type": "button",
+        "value": "ruby"
+    }, {
+        "name": "webdev",
+        "text": "Web Dev",
+        "type": "button",
+        "value": "webdev"
+    }
+
+]
+
+base_resources = {
+    "text": "We recommend the following resources.",
+    "attachments": [
+        {
+            "text": "",
+            "fallback": "",
+            "color": "#3AA3E3",
+            "callback_id": "resource_buttons",
+            "attachment_type": "default",
+            "actions": None  # add in builder step
+        },
+        {
+            "text": "",
+            "fallback": "",
+            "color": "#3AA3E3",
+            "callback_id": "suggestion",
+            "attachment_type": "default",
+            "actions": [
+                {
+                    "name": "suggestion_button",
+                    "text": "Are we missing something? Click!",
+                    "type": "button",
+                    "value": "suggestion_button"
+                }
+            ]
+        }
+    ]
+}
