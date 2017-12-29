8f9b0ce317cc5a5098c88361d0a2336f73873f8b
William Montgomery <wimo7083@colorado.edu>
2017-12-29 16:15:34 -0700

    removed debugging calls, included util functions.

--- ocbot/external/all.py
+++ ocbot/external/all.py
@@ -10,19 +10,7 @@ def external_router(calls: List[ResponseContainer]):
     # slack, or airtable
     for call in calls:
         cls = getattr(sys.modules[__name__], call.route)()
-
         class_method = getattr(cls, call.method)
         yield class_method(call.payload)
 
 
-
-print(__name__)
-if __name__ == '__main__':
-
-
-    part_one = ResponseContainer(route='Slack', method='chat.update', payload=dict(cat='dog'))
-
-    #SlackStart(api_key='test', verification_token='test')
-
-    part_two = ResponseContainer(route='Slack', method='user_name_from_id', payload=dict(mike='mike'))
-    external_router([part_one, part_two ])
--- ocbot/external/route_slack.py
+++ ocbot/external/route_slack.py
@@ -73,3 +73,95 @@ class Slack:
         print(f'default found.... {method}, {payload}')
         response = _slack_config['client'].api_call(method, **payload)
 
+    def is_slack_success(response_list: List[ResponseContainer], event_key: UUID) -> bool:
+
+        for item in response_list:
+            try:
+                client.api_call(item.call_method, **item.response)
+                yield True
+            except Exception as response:
+                logging.exception(response, UUID)
+                yield False
+
+
+def build_message(message_template: str, **kwargs: dict) -> str:
+    return message_template.format(**kwargs)
+
+
+def get_response_type(response_data):
+    return response_data['actions'][0]['value']
+
+
+def join_channels():
+    """
+    Utility function for joining channels.  Move to utils?
+    """
+    client.api_call('channels.join', name='general')
+
+
+def make_base_params(data, text_value, ):
+    return {'text': text_value,
+            'channel': data['channel']['id'],
+            'ts': data['message_ts'],
+            'as_user': True
+            }
+
+
+def user_name_from_id(user_id: str) -> str:
+    """
+    Queries the Slack workspace for t   he users real name
+    to personalize messages.  Prioritizes real_name -> name -> 'New Member'
+    :param user_id:
+    """
+    response = client.api_call('users.info', user=user_id)
+
+    if response['user']['real_name']:
+        return response['user']['real_name'].title()
+    elif response['user']['name']:
+        return response['user']['name'].title()
+    else:
+        return 'New Member'
+
+
+def list_channels():
+    channels_call = client.api_call("channels.list")
+    return channels_call['channels'] if channels_call.get('ok') else None
+
+
+def print_channels():
+    channels = list_channels()
+    if channels:
+        print("Channels: ")
+        for channel in channels:
+            print(channel['name'] + " (" + channel['id'] + ")")
+            # detailed_info = channel_info(channel['id'])
+            # if detailed_info:
+            #     if detailed_info['members']:
+            #         print([x for x in detailed_info['members']])
+            # print('Latest text from ' + channel['name'] + ":")
+            # print(detailed_info['latest']['text'])
+        print('-----')
+    else:
+        print("Unable to authenticate.")
+
+
+def send_message(channel_id, message):
+    client.api_call(
+        "chat.postMessage",
+        channel=channel_id,
+        text=message,
+        username='test-bot',
+        icon_emoji=':robot_face:'
+    )
+
+
+def get_bot_id(bot_name):
+    api_call = client.api_call('users.list')
+    if api_call.get('ok'):
+        # retrieve all users so we can find our bot
+        users = api_call.get('members')
+        for user in users:
+            if 'name' in user and user.get('name') is bot_name:
+                print(f"Bot ID for {user['name']} is {user.get('id')}")
+    else:
+        print(f"Could not find bot user with the name {bot_name}")
