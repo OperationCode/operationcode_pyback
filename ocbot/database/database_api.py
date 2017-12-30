fcbf3e22621d502d514c5bfbb089faa20d184f3e
AllenAnthes <abanthes@gmail.com>
2017-12-29 20:02:02 -0600

    Interests mostly working for add/update_user. Currently can only append new ones

--- ocbot/database/database_api.py
+++ ocbot/database/database_api.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from ocbot.database.models import Base, User, Interest
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 
 
 class PyBotDatabase:
@@ -20,10 +20,9 @@ class PyBotDatabase:
         Pre-populates the interests table
         """
         session = sessionmaker(bind=self.engine)()
-        interests = ["Java", "Javascript", "Ruby", "Python", "C#", "HTML / CSS", "C", "Web Developer", "Web Designer",
-                     "Front-End Developer", "Back-End Developer", "Full-Stack Developer", "Mobile / IOS",
-                     "Mobile / Android",
-                     "Cyber Security", "Data Science"]
+        interests = ["Javascript", "Ruby", "Java", "Python", "C#", "C", "Swift", ".NET", "HTML / CSS", "Mobile / IOS",
+                     "Full-Stack Developer", "Data Science", "Back-End Developer", "Front-End Developer",
+                     "Cyber Security", "I.T / SysAdmin", "Web Designer", "Web Developer", "Mobile / Android"]
 
         for interest in interests:
             session.add(Interest(name=interest))
@@ -56,13 +55,18 @@ class PyBotDatabase:
         :param kwargs:
         """
         session = sessionmaker(bind=self.engine)()
-        res = session.add(User(**kwargs))
+        interests, kwargs = PyBotDatabase._slice_interests(kwargs)
+        new_user = User(**kwargs)
+        res = session.add(new_user)
+        if interests:
+            # user = session.query(User).filter_by(email=new_user.email)
+            self._handle_interests(new_user, session, interests)
         session.commit()
         return res
 
     def update_user(self, email: str, **kwargs: dict) -> bool:
         """
-        TODO: This will not handle appending new interests correctly
+        TODO: Currently can only append new interests, not delete
 
         Updates a user with the given email in the users table
         Fields to be updated should be given in the form of a dict
@@ -78,9 +82,48 @@ class PyBotDatabase:
             Returns true if a record was successfully updated
         """
         session = sessionmaker(bind=self.engine)()
-        num_records = session.query(User).filter_by(email=email).update({**kwargs})
+        user = session.query(User).filter_by(email=email)
+        interests, kwargs = PyBotDatabase._slice_interests(kwargs)
+
+        num_records = user.update({**kwargs})
+        if interests:
+            self._handle_interests(user.first(), session, interests)
+
         session.commit()
         return bool(num_records)
+
+    @classmethod
+    def _slice_interests(cls, kwargs: dict) -> tuple:
+        """
+        Utility method to check if the list of interests were supplied in the kwargs,
+        If 'interests' key is present method returns a tuple containing the list of interests as strings
+        and the remaining kwargs.
+        If 'interests' key is absent the 0th index is returned None
+
+        :param kwargs:
+        :return:
+        """
+        if 'interests' in kwargs:
+            interests = kwargs['interests']
+            new_kwargs = {key: val for key, val in kwargs.items() if key != 'interests'}
+            return interests, new_kwargs
+        return None, kwargs
+
+    @staticmethod
+    def _handle_interests(user: User, session: Session, interests: list) -> None:
+        """
+        Utility method for appending interests to User object
+
+        :param user:
+        :param session:
+        :param interests:
+        """
+        for interest in interests:
+            interest_object = session.query(Interest).filter_by(name=interest).first()
+
+            #  Null check, simply skips undefined interests
+            if interest_object:
+                user.interests.append(interest_object)
 
     def delete_user(self, email: str) -> bool:
         """
@@ -96,12 +139,18 @@ class PyBotDatabase:
 
 
 if __name__ == '__main__':
+    """
+    Stuff for lazy testing.
+    """
     db = PyBotDatabase()
     params = {
         'first_name': 'Bob',
         'last_name': 'Boberson',
-        # 'email': 'fake@email.com'
+        'interests': ['fghfgh'],
     }
     num = db.update_user('fake@email.com', **params)
     # num = db.add_user(**params)
     print(num)
+
+    test_session = sessionmaker(bind=db.engine)()
+    print(test_session.query(User).filter_by(last_name='Boberson').first().interests)
