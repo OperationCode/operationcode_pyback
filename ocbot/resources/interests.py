f419e77b2bc0c84ae189a4e3f1eff362d1e7cdb9
wimo7083 <wimo7083@colorado.edu>
2018-01-03 10:45:29 -0700

    converted to dict with membership.

--- ocbot/resources/interests.py
+++ ocbot/resources/interests.py
@@ -1,25 +1,41 @@
 import yaml
-interests = ["Javascript", "Ruby", "Java", "Python", "C#", "C", "Swift",
-            ".NET", "HTML / CSS", "Mobile / IOS", "Full-Stack Developer", 
-            "Data Science", "Back-End Developer", "Front-End Developer",
-            "Cyber Security", "I.T / SysAdmin", "Web Designer", 
-            "Web Developer", "Mobile / Android"
-            ]
+from collections import defaultdict
 
-interests = [item.lower() for item in interests]
+def iter_membership(resource_dict_list):
+    values = ['category', 'language']
+    for resource_dict in resource_dict_list:
+        for item in values:
+            try:
+                if resource_dict[item].lower() in interests:
+                    yield item, resource_dict
+            except KeyError:
+                print('keyerror')
+                pass
+            except AttributeError:
+                # nonetype for item.lower()
+                pass
 
-loaded = None
-with open('resources.yml', 'r') as input:
-    try:
-        loaded = yaml.load(input)
-    except yaml.YAMLError as exc:
-        print(exc)
+def load_yaml(input_name):
+    with open(input_name, 'r') as input:
+        try:
+            return yaml.load(input)
+        except yaml.YAMLError as exc:
+            print(exc)
 
-segmented = []
-for item in loaded:
-    if item['category'].lower() or item['language'].lower() in interests:
-        segmented.append(item)
-    else:
-        print(item)
+def build_item_dict(yaml_list):
+    split = defaultdict(list)
+    for found_key, dict_found in iter_membership(yaml_list):
+        split[dict_found[found_key]].append(dict_found)
 
+    print(split.keys())
 
+if __name__ == '__main__':
+    interests = ["Javascript", "Ruby", "Java", "Python", "C#", "C", "Swift",
+                 ".NET", "HTML / CSS", "Mobile / IOS", "Full-Stack Developer",
+                 "Data Science", "Back-End Developer", "Front-End Developer",
+                 "Cyber Security", "I.T / SysAdmin", "Web Designer",
+                 "Web Developer", "Mobile / Android"
+                 ]
+
+    interests = [item.lower() for item in interests]
+    build_item_dict(load_yaml('resources.yml'))
