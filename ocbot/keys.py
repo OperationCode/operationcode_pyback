179017336cdaa041220d7cb342ef3fa70c24a79a
AllenAnthes <abanthes@gmail.com>
2018-01-05 10:43:15 -0600

    added default fake creds for testing

--- ocbot/keys.py
+++ ocbot/keys.py
@@ -2,18 +2,18 @@ from decouple import config
 
 # app verification tokens
 # VERIFICATION_TOKEN = config('OPCODE_VERIFICATION_TOKEN')
-VERIFICATION_TOKEN = config('APP_VERIFICATION_TOKEN')
+VERIFICATION_TOKEN = config('APP_VERIFICATION_TOKEN', default='token')
 # VERIFICATION_TOKEN = config('TEST2_AUTH_TOKEN')
 
 # from src.airtable_handling import airtable
 
 # Workspace token selection
-TOKEN = config('PERSONAL_APP_TOKEN')
+TOKEN = config('PERSONAL_APP_TOKEN', default='token')
 # TOKEN = config('TEST2_BOT_TOKEN')
 # TOKEN = config('OPCODE_APP_TOKEN')
 
 # community channel selections
-COMMUNITY_CHANNEL = config('PERSONAL_PRIVATE_CHANNEL')
+COMMUNITY_CHANNEL = config('PERSONAL_PRIVATE_CHANNEL', default='community_channel')
 # COMMUNITY_CHANNEL = config('TEST2_PRIVATE_CHANNEL')
 # COMMUNITY_CHANNEL = config('OPCODE_REWRITE_CHANNEL')
 # PROJECTS_CHANNEL = config('OPCODE_OC_PROJECTS_CHANNEL')
@@ -21,8 +21,8 @@ COMMUNITY_CHANNEL = config('PERSONAL_PRIVATE_CHANNEL')
 # COMMUNITY_CHANNEL = config('OPCODE_BOT_TESTING_CHANNEL')
 
 # airtable config selectrions
-AIRTABLE_BASE_KEY = config('PERSONAL_AIRTABLE_BASE_KEY')
-AIRTABLE_API_KEY = config('PERSONAL_AIRTABLE_TOKEN')
+AIRTABLE_BASE_KEY = config('PERSONAL_AIRTABLE_BASE_KEY', default='fake_airtable_base')
+AIRTABLE_API_KEY = config('PERSONAL_AIRTABLE_TOKEN', default='fake_airtable_key')
 AIRTABLE_TABLE_NAME = 'Mentor Request'
 #AIRTABLE_BASE_KEY = config('OPCODE__AIRTABLE_BASE_KEY')
 #AIRTABLE_API_KEY = config('OPCODE_AIRTABLE_TOKEN')
