a626fbba35db5da089dc5d6718882e504c1b8555
AllenAnthes <abanthes@gmail.com>
2018-02-24 15:35:18 -0600

    added default config environment

--- config/development.py
+++ config/development.py
@@ -2,18 +2,18 @@ from decouple import config
 
 PORT = 5000
 
-TOKEN = config('TEST2_BOT_TOKEN', default='token')
-VERIFICATION_TOKEN = config('TEST2_AUTH_TOKEN', default='token')
-COMMUNITY_CHANNEL = config('TEST2_PRIVATE_CHANNEL', default='community_channel')
-MENTORS_INTERNAL_CHANNEL = config('TEST2_PRIVATE_CHANNEL', default='mentor_channel')
+TOKEN = config('DEV_BOT_TOKEN', default='token')
+VERIFICATION_TOKEN = config('DEV_AUTH_TOKEN', default='token')
+COMMUNITY_CHANNEL = config('DEV_PRIVATE_CHANNEL', default='community_channel')
+MENTORS_INTERNAL_CHANNEL = config('DEV_PRIVATE_CHANNEL', default='mentor_channel')
 
-AIRTABLE_BASE_KEY = config('PERSONAL_AIRTABLE_BASE_KEY', default='fake_airtable_base')
-AIRTABLE_API_KEY = config('PERSONAL_AIRTABLE_TOKEN', default='fake_airtable_key')
+AIRTABLE_BASE_KEY = config('DEV_AIRTABLE_BASE_KEY', default='fake_airtable_base')
+AIRTABLE_API_KEY = config('DEV_AIRTABLE_TOKEN', default='fake_airtable_key')
 AIRTABLE_TABLE_NAME = 'Mentor Request'
 
-PG_USERNAME = config('PERSONAL_PG_USERNAME', default='username')
-PG_PASSWORD = config('PERSONAL_PG_PASSWORD', default='password')
-PG_URL = config('PERSONAL_PG_URL', default='url')
+PG_USERNAME = config('DEV_PG_USERNAME', default='username')
+PG_PASSWORD = config('DEV_PG_PASSWORD', default='password')
+PG_URL = config('DEV_PG_URL', default='url')
 
 PA_SSH_USERNAME = config('PA_SSH_USERNAME', default=None)
 PA_SSH_PASSWORD = config('PA_SSH_PASSWORD', default=None)
--- run.py
+++ run.py
@@ -1,4 +1,8 @@
-from ocbot.web.app import start_server
+import os
+
+if 'CONFIG_FILE' not in os.environ:
+    os.environ['CONFIG_FILE'] = 'development.py'
 
 if __name__ == '__main__':
+    from ocbot.web.app import start_server
     start_server()
