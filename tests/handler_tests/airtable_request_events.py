663294df85d2446a61f4a265f6aeae6e1f5ebc3c
AllenAnthes <abanthes@gmail.com>
2018-03-18 15:31:19 -0500

    added tests for mentee claim button

--- tests/handler_tests/airtable_request_events.py
+++ tests/handler_tests/airtable_request_events.py
@@ -47,23 +47,66 @@ TEXT_DICT_DETAILS = 'Additional details:  Lorem ipsum dolor sit amet, consectetu
                     'tempor felis. Proin vel cursus tellus. Quisque eget mauris neque. Cras eu pharetra leo. Nam ' \
                     'nulla tortor, imperdiet sit amet dictum eu, mollis id ante.'
 
-
 MENTOR_REQUEST_ATTACHMENT = [
-        {
-            'text': '',
-            'fallback': '',
-            'color': '#3AA3E3',
-            'callback_id': 'claim_mentee',
-            'attachment_type': 'default',
-            'actions': [
-                {
-                    'name': 'fakerec',
-                    'text': 'Claim Mentee',
-                    'type': 'button',
-                    'style': 'primary',
-                    'value': f'mentee_claimed',
-                }
-            ]
+    {
+        'text': '',
+        'fallback': '',
+        'color': '#3AA3E3',
+        'callback_id': 'claim_mentee',
+        'attachment_type': 'default',
+        'actions': [
+            {
+                'name': 'fakerec',
+                'text': 'Claim Mentee',
+                'type': 'button',
+                'style': 'primary',
+                'value': f'mentee_claimed',
+            }
+        ]
 
-        }
-    ]
+    }
+]
+
+CLAIM_MENTEE_EVENT = {'type': 'interactive_message',
+                      'actions': [{'name': 'rec7pRh2FwyO4nP2W', 'type': 'button', 'value': 'mentee_claimed'}],
+                      'callback_id': 'claim_mentee', 'team': {'id': 'T8M8SQEN7', 'domain': 'test'},
+                      'channel': {'id': 'G8NDRJJF9', 'name': 'privategroup'},
+                      'user': {'id': 'U11111', 'name': 'tester'},
+                      'action_ts': '1521402127.915363', 'message_ts': '1521402116.000129', 'attachment_id': '1',
+                      'token': 'faketoken', 'is_app_unfurl': False, 'original_message': {
+        'text': 'User <@U8N6XBL7Q> has requested a mentor for General Guidance - Slack Chat\n\nGiven Skillset(s): None given\n\nView requests: <https://airtable.com/tbl9uQEE8VeMdNCey/viwYzYa4J9aytVB4B|Airtable>',
+        'username': 'test2-bot', 'bot_id': 'B8N6Z8M8E',
+        'attachments': [
+            {'callback_id': 'claim_mentee', 'id': 1, 'color': '3AA3E3', 'actions': [
+                {'id': '1', 'name': 'rec7pRh2FwyO4nP2W', 'text': 'Claim Mentee', 'type': 'button',
+                 'value': 'mentee_claimed', 'style': 'primary'}]}], 'type': 'message', 'subtype': 'bot_message',
+        'thread_ts': '1521402116.000129', 'reply_count': 1, 'replies': [{'user': 'B00', 'ts': '1521402117.000015'}],
+        'subscribed': False, 'unread_count': 1, 'ts': '1521402116.000129'},
+                      'response_url': 'https://hooks.slack.com/actions/T8M8SQEN7/332727073942/JPnXPwSk8A5jffzf0DHuSnhS',
+                      'trigger_id': '331226307360.293298830755.c73cad81aa525200275c2868dd168ab0'}
+
+RESET_MENTEE_CLAIM_EVENT = {'type': 'interactive_message',
+                            'actions': [{'name': 'rec7pRh2FwyO4nP2W', 'type': 'button', 'value': 'reset_claim_mentee'}],
+                            'callback_id': 'claim_mentee', 'team': {'id': 'T8M8SQEN7', 'domain': 'test'},
+                            'channel': {'id': 'G8NDRJJF9', 'name': 'privategroup'},
+                            'user': {'id': 'U11111', 'name': 'tester'}, 'action_ts': '1521403472.901817',
+                            'message_ts': '1521402116.000129', 'attachment_id': '1',
+                            'token': 'faketoken', 'is_app_unfurl': False, 'original_message': {
+        'text': 'User <@U8N6XBL7Q> has requested a mentor for General Guidance - Slack Chat\n\nGiven Skillset(s): None given\n\nView requests: &lt;https://airtable.com/tbl9uQEE8VeMdNCey/viwYzYa4J9aytVB4B|Airtable&gt;',
+        'username': 'test2-bot', 'bot_id': 'B8N6Z8M8E', 'attachments': [{'callback_id': 'claim_mentee',
+                                                                         'text': ':100: Request claimed by <@U8N6XBL7Q>:100:\n<!date^1521402131^Greeted at {date_num} {time_secs}|Failed to parse time>',
+                                                                         'id': 1, 'color': '3AA3E3', 'actions': [
+                {'id': '1', 'name': 'rec7pRh2FwyO4nP2W', 'text': 'Reset claim', 'type': 'button',
+                 'value': 'reset_claim_mentee', 'style': 'danger'}]}], 'type': 'message', 'subtype': 'bot_message',
+        'thread_ts': '1521402116.000129', 'reply_count': 1, 'replies': [{'user': 'B00', 'ts': '1521402117.000015'}],
+        'subscribed': False, 'unread_count': 1, 'ts': '1521402116.000129'},
+                            'response_url': 'https://hooks.slack.com/actions/T8M8SQEN7/331230500336/J5MfM0OC6I37iV9xQSPqQ2UD',
+                            'trigger_id': '332007136373.293298830755.614be10d27ebd4e3d22af708906f27e0'}
+
+INVALID_MENTOR_ID_TEXT = f":warning: <@U11111> not found in Mentor table. :warning:"
+
+RESET_MENTEE_ATTACHMENT = [{'text': 'Reset by <@U11111> at <!date^11111^ {date_num} {time_secs}|Failed to parse time>',
+                            'fallback': '', 'color': '#3AA3E3', 'callback_id': 'claim_mentee',
+                            'attachment_type': 'default', 'actions': [
+        {'name': 'rec7pRh2FwyO4nP2W', 'text': 'Claim Mentee', 'type': 'button', 'style': 'primary',
+         'value': 'mentee_claimed'}]}]
---
+++ tests/handler_tests/test_claim_mentee.py
@@ -0,0 +1,91 @@
+import pytest
+import pytest_mock
+from ocbot.external.route_airtable import AirTableBuilder
+from ocbot.pipeline.handlers.claim_mentee import MenteeClaimHandler
+from tests.handler_tests.airtable_request_events import CLAIM_MENTEE_EVENT, INVALID_MENTOR_ID_TEXT, \
+    RESET_MENTEE_CLAIM_EVENT, RESET_MENTEE_ATTACHMENT
+
+FAKE_RECORD_ID = 'rec123'
+
+AIRTABLE_MENTOR_ID_CALL = 'ocbot.external.route_airtable.Airtable.mentor_id_from_slack_username'
+
+
+@pytest.fixture
+def claim_handler():
+    return MenteeClaimHandler(event_dict=CLAIM_MENTEE_EVENT)
+
+
+@pytest.fixture
+def reset_handler():
+    return MenteeClaimHandler(event_dict=RESET_MENTEE_CLAIM_EVENT)
+
+
+def test_mentee_claim_handler_initializes_without_error(claim_handler: MenteeClaimHandler):
+    pass
+
+
+def test_has_correct_user_id(claim_handler: MenteeClaimHandler):
+    assert claim_handler._user_id == CLAIM_MENTEE_EVENT['user']['id']
+
+
+def test_has_correct_user_name(claim_handler: MenteeClaimHandler):
+    assert claim_handler._user_name == CLAIM_MENTEE_EVENT['user']['name']
+
+
+def test_correctly_stores_event(claim_handler: MenteeClaimHandler):
+    assert claim_handler._event == CLAIM_MENTEE_EVENT
+
+
+def test_has_correct_click_type_when_claim(claim_handler: MenteeClaimHandler):
+    assert claim_handler.click_type == CLAIM_MENTEE_EVENT['actions'][0]['value']
+
+
+def test_has_correct_record_id(claim_handler: MenteeClaimHandler):
+    assert claim_handler._record_id == CLAIM_MENTEE_EVENT['actions'][0]['name']
+
+
+def test_calls_airtable_for_mentor_id(mocker: pytest_mock, claim_handler: MenteeClaimHandler):
+    mock = mocker.patch(AIRTABLE_MENTOR_ID_CALL, return_value=FAKE_RECORD_ID)
+
+    claim_handler.api_calls()
+    assert mock.called
+    assert claim_handler.api_dict['mentor_id'] == FAKE_RECORD_ID
+
+
+def test_mentee_claimed_attachment_called_when_has_valid_mentor_id(mocker: pytest_mock,
+                                                                   claim_handler: MenteeClaimHandler):
+    claim_handler.api_dict['mentor_id'] = FAKE_RECORD_ID
+    spy = mocker.spy(claim_handler, 'mentee_claimed_attachments')
+    claim_handler.build_templates()
+    assert spy.called
+
+
+def test_correct_attachment_when_invalid_mentor_id(mocker: pytest_mock,
+                                                   claim_handler: MenteeClaimHandler):
+    claim_handler.api_dict['mentor_id'] = ''
+    spy = mocker.spy(claim_handler, 'mentee_unclaimed_attachment')
+    claim_handler.build_templates()
+    assert spy.called
+    assert claim_handler.text_dict['message']['attachments'][0]['text'] == INVALID_MENTOR_ID_TEXT
+
+
+def test_has_correct_click_type_when_reset(reset_handler: MenteeClaimHandler):
+    assert reset_handler.click_type == RESET_MENTEE_CLAIM_EVENT['actions'][0]['value']
+
+
+def test_has_correct_attachment_when_reset(mocker: pytest_mock, reset_handler: MenteeClaimHandler):
+    mocker.patch.object(reset_handler, 'now', return_value=11111)
+    reset_handler.api_dict['mentor_id'] = FAKE_RECORD_ID
+    reset_handler.build_templates()
+    assert reset_handler.text_dict['message']['attachments'] == RESET_MENTEE_ATTACHMENT
+
+
+def test_build_correct_response_when_mentee_claimed(mocker: pytest_mock, claim_handler: MenteeClaimHandler):
+    claim_handler.api_dict['mentor_id'] = FAKE_RECORD_ID
+    claim_handler.text_dict['message'] = {}
+    record = CLAIM_MENTEE_EVENT['actions'][0]['name']
+    correct_airtable_response = AirTableBuilder.claim_mentee(record, FAKE_RECORD_ID)
+
+    claim_handler.build_responses()
+
+    assert correct_airtable_response in claim_handler.response
