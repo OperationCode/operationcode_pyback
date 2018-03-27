18fe75deb14bf4c62bc8cca493831d41406a7c9f
wimo7083 <wimo7083@colorado.edu>
2018-03-26 18:34:55 -0600

    updated requirements.txt

--- ocbot/pipeline/slash_command_handlers/lunch_handler.py
+++ ocbot/pipeline/slash_command_handlers/lunch_handler.py
@@ -7,12 +7,11 @@ import logging
 logger = logging.getLogger(__name__)
 
 
-
 def random_zip():
     random_zip = 0
     while not is_valid(str(random_zip)):
-        range_start = 10**(4)
-        range_end = (10**5)-1
+        range_start = 10 ** (4)
+        range_end = (10 ** 5) - 1
         random_zip = randint(range_start, range_end)
 
     return str(random_zip)
@@ -21,23 +20,24 @@ def random_zip():
 def within_lunch_range(input_number):
     return int(input_number) <= 30
 
+
 def two_params(first_param, second_param):
     if is_valid(first_param) and within_lunch_range(second_param):
         return {'location': first_param, 'range': second_param}
     else:
         return {'location': random_zip(), 'range': '20'}
 
+
 def split_params(param_text):
-    print(param_text)
-    if not param_text: # no params, default random zip code, 20 miles
+    if not param_text:  # no params, default random zip code, 20 miles
         return {'location': random_zip(), 'range': '20'}
 
     params = param_text.split()
 
-    if len(params) == 2: # two values
+    if len(params) == 2:  # two values
         return two_params(params[0], params[1])
 
-    if len(params) == 1 and is_valid(params[0]): # one value
+    if len(params) == 1 and is_valid(params[0]):  # one value
         return {'location': params[0], 'range': '20'}
 
     else:
@@ -47,8 +47,9 @@ def split_params(param_text):
 def get_random_lunch(lunch_response):
     number_locs = len(lunch_response['businesses'])
 
-    selected_loc = randint(0, number_locs-1)
+    selected_loc = randint(0, number_locs - 1)
     return lunch_response['businesses'][selected_loc]
+
 
 def build_response_text(loc_dict):
     return f'The Wheel of Lunch has selected {loc_dict["name"]} at {" ".join(loc_dict["location"]["display_address"])}'
@@ -67,8 +68,6 @@ def create_lunch_event(request):
     return build_response_text(loc)
 
 
-
 if __name__ == '__main__':
-    print(create_lunch_event({'text':'80020 20'}))
-    print(create_lunch_event({'text':'20'}))
-
+    print(create_lunch_event({'text': '80020 20'}))
+    print(create_lunch_event({'text': '20'}))
