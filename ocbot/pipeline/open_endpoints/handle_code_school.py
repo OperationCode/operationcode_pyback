4b0c9a74acbe902f849cbb973a0ed65234246a09
William Montgomery <wimo7083@colorado.edu>
2018-04-05 21:31:46 -0600

    include handler

---
+++ ocbot/pipeline/open_endpoints/handle_code_school.py
@@ -0,0 +1,16 @@
+import datetime
+import os
+
+
+from flask import url_for, redirect, render_template
+
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def handle_code_school():
+
+
+    return render_template("code_school.html")
